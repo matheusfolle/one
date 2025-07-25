5dcd4c0071e9dac2b0d3a2e956eb392c9e417825
Matheus Folle <mathfolle@icloud.com>
2025-07-25 14:34:14 -0300

    feat: adiciona novos desafios do curso 2 de lógica de programação: explorando funções e listas

--- prog_iniciante/logica_js2/js-curso-2/desafios.js
+++ prog_iniciante/logica_js2/js-curso-2/desafios.js
@@ -1,4 +1,4 @@
-// Desafios dia 2
+// Desafios dia 2 - Oracle Next Education
 
 // // Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio:
 
@@ -93,7 +93,100 @@
 
 // console.log(quadradoDoNumero(5));
 
+// Desafios 3
 
+// // Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
 
+// function indiceDeMassa(altura, peso) {
+//   return Number(peso) / Number((altura) * Number(altura));
+// }
+
+// console.log(indiceDeMassa(176, 77));
+
+// // Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+
+// function fatorialDoNum(num) {
+//   let contador = 1;
+//   let resultado = 1;
+//   while(contador <= num){
+//     resultado *= contador;
+//     contador++;
+//   }
+//   return resultado;
+// }
+
+// // Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
+
+// function valorEmDolar(dolar){
+//   dolar = Number(dolar);
+//   let resultado = 0;
+//   let cotacaoDolar = 4.80;
+//   resultado = dolar * cotacaoDolar;
+//   return resultado.toFixed(2)
+// }
+
+// console.log(valorEmDolar(15));
+
+// // Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.
+
+// function calculaArea(altura, largura) {
+//   return Number(altura) * Number(largura);
+// }
+
+// function calculaPerimetro(altura, largura) {
+//   return (Number(altura) + Number(largura)) * 2;
+// }
+
+// // Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
+
+// function areaCircular(raio) {
+//   let area = Math.PI * (Number(raio) * Number(raio)); 
+//   let perimetro = (2 * Math.PI) * Number(raio);
+
+//   console.log(`Área da sala circular: ${area.toFixed(2)} metros quadrados`);
+//   console.log(`Perímetro da sala circular: ${perimetro.toFixed(2)} metros`)
+// }
+
+// areaCircular(6);
+
+// // Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
+
+// function tabuada(num) {
+//   let contador = 1;
+//   while(contador <= 10) {
+//     let resultado = Number(num) * contador;
+//     console.log(`${num} x ${contador} = ${resultado}`);
+//     contador++;
+//   }
+// }
+
+// tabuada(8)
+
+// Desafios 4 
+// // Crie uma lista vazia, com o nome listaGenerica.
+
+// let listaGenerica = [];
+
+// // Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+
+// let listaLinguagens = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+// // Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+
+// listaLinguagens.push("Java", "Ruby", "GoLang");
+// console.log(listaLinguagens);
+
+// // Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+
+// let listaTresElementos = ["Danone", "Yakult", "Bebida Proteica"];
+// console.log(listaTresElementos[0]);
+
+// // Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+
+// console.log(listaTresElementos[1]);
+
+// // Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+
+// console.log(listaTresElementos[2]);
 
 
